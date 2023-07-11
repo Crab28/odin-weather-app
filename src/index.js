807b79df5387d4e2d2136abfad0d154f0cb59494
Crab <crabcodes28@gmail.com>
2023-07-11 13:53:25 +0200

    Add search bar
    Search bar can call the service and update the page

--- src/index.js
+++ src/index.js
@@ -1,9 +1,22 @@
 import './styles.css';
 import getWeather from './service';
 
-const initializePage = function applyInitialLocation(location) {
+const updatePage = function updatePageWithLocation(location) {
   getWeather(location)
     .then((result) => console.log(result));
 };
 
-initializePage('pretoria');
+const initializeListeners = function initializeListeners() {
+  const searchBar = document.getElementById('location-search');
+
+  searchBar.addEventListener('keypress', (event) => {
+    if (event.key === 'Enter') {
+      event.preventDefault();
+      updatePage(searchBar.value);
+      searchBar.value = '';
+    }
+  });
+};
+
+updatePage('pretoria');
+initializeListeners();
