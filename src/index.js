7ba8dcdc5959d30e9b9059e19918a219cd36806e
Crab <crabcodes28@gmail.com>
2023-07-11 12:42:14 +0200

    Add fetch weather function

--- src/index.js
+++ src/index.js
@@ -1,1 +1,12 @@
-console.log('works');
+import './styles.css';
+
+const getWeather = async function fetchWeatherData(location) {
+  const KEY = '0d9d028bd7cf44ec9fe94100231107';
+  const response = await fetch(`https://api.weatherapi.com/v1/current.json?key=${KEY}&q=${location}`);
+  const weatherData = await response.json();
+
+  return weatherData;
+};
+
+getWeather('london')
+  .then((result) => console.log(result));
